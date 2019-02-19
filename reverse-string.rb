0c50dd0e0faf5584048c3999854749c7e54b46e3
Roxanne Cojocariu <rmcojocariu@gmail.com>
2019-02-19 15:41:27 -0500

    reverse-string

---
+++ reverse-string.rb
@@ -0,0 +1,9 @@
+require 'pry'
+def reverse_string(str)
+  split_string = str.split('')
+  reversed_string = []
+  split_string.length.times do |letter|
+    reversed_string << split_string.pop
+  end
+  return reversed_string.join('')
+end
